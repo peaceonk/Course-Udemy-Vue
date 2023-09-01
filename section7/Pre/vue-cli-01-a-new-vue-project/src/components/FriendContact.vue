46d8210b6c7451abcb473b402b2ceb12e54805ba
Peace <peaceonk@gmail.com>
2023-09-01 10:50:25 +0900

    complete section7

---
+++ section7/Pre/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <h2> {{ friend.name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-show="detailsAreVisible">
+      <li>
+        <strong>Phone : </strong> {{ friend.phone }}
+      </li>
+      <li>
+        <strong>Email : </strong> {{ friend.email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 'peace',
+        name: 'peace kim',
+        phone: '01011112222',
+        email: 'peace@peace.kim'
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  },
+}
+</script>
