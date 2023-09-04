21da2420ec78bf8990c227226a238d19adbd58ec
Peace <peaceonk@gmail.com>
2023-09-04 16:57:53 +0900

    complete section8

--- section8/Pre/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeElement.vue
+++ section8/Pre/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeElement.vue
@@ -2,7 +2,8 @@
   <li>
     <h3>{{ topicName }}</h3>
     <p>{{ description }}</p>
-    <button @click="$emit('select-topic', id)">Learn More</button>
+    <!-- <button @click="$emit('select-topic', id)">Learn More</button> -->
+    <button @click="selectTopic(id)">Learn More</button>  <!-- app.vue에서 provide한 selectTopic -->
   </li>
 </template>
 
@@ -10,5 +11,6 @@
 export default {
   props: ['id', 'topicName', 'description'],
   emits: ['select-topic'],
+  inject: ['selectTopic']
 };
 </script>
--- section8/Pre/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
+++ section8/Pre/cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -1,19 +1,27 @@
 <template>
   <ul>
-    <knowledge-element
+    <!-- <knowledge-element
       v-for="topic in topics"
       :key="topic.id"
       :id="topic.id"
       :topic-name="topic.title"
       :description="topic.description"
       @select-topic="$emit('select-topic', $event)"
+    ></knowledge-element> -->
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
     ></knowledge-element>
   </ul>
 </template>
 
 <script>
 export default {
-  props: ['topics'],
-  emits: ['select-topic']
+  // props: ['topics'],
+  inject: ['topics'],
+  // emits: ['select-topic']
 };
 </script>
